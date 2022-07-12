2bcc6278af8a7f9713a5bc64a05a76a3d52ab233
Marina Gonzalez Dante <mgonzalezdante@hotmail.com>
2022-07-11 22:08:26 -0300

    avances

---
+++ Handlebars/server.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import { engine } from 'express-handlebars';
+import Container from './Container.js'
+
+const app = express()
+
+app.engine('hbs', engine({
+    extname: 'hbs',
+    defaultLayout: 'index.hbs',
+    layoutsDir: "./views/layouts",
+    partialsDir: "./views/partials"
+}));
+app.set('view engine', 'hbs')
+app.set('views', "./views")
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const productContainer = new Container()
+
+app.get('', (req, res) => {
+    res.render('form');
+})
+
+app.get('/productos', (req, res) => {
+    res.render("products", { products: productContainer.getAllItems() });
+});
+
+app.post('/productos', (req, res) => {
+    const newProduct = req.body;
+    productContainer.add(newProduct);
+    console.log(newProduct)
+    console.log(`se agrego un producto, cantidad total: ${productContainer.getAllItems().length}`)
+    res.redirect('..');
+})
+
+app.use(express.static('public'))
+
+const PORT = process.env.PORT || 8080
+const server = app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${server.address().port}`)
+})
+
+server.on('error', error => console.log(`Error en servidor ${error}`))
